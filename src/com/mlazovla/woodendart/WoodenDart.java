4b6f5c35e24b5de283e4930cc67e6f35f7d9e168
Vladimír Mlázovský <vml@seznam.cz>
2014-01-21 13:52:18 +0100

    Project sets
    Simple Android app to counting Dart score.

---
+++ src/com/mlazovla/woodendart/WoodenDart.java
@@ -0,0 +1,23 @@
+package com.mlazovla.woodendart;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WoodenDart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wooden_dart);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.wooden_dart, menu);
+        return true;
+    }
+    
+}
